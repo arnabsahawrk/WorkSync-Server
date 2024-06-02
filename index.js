7b0e6c7a90dd7a1715f0e48eb52e7df8035a6ff7
arnabw <arnabsahawrk@gmail.com>
2024-06-03 03:34:05 +0600

    added jwt and new user api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 
 //config
 require("dotenv").config();
@@ -12,6 +13,28 @@ app.use(cors());
 app.use(express.json());
 
 //custom middleware
+//token verifying middleware
+const verifyToken = (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_KEY, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+
+    req.decoded = decoded;
+    next();
+  });
+};
 
 //Database Authenticate
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2p5zaxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -28,9 +51,77 @@ const client = new MongoClient(uri, {
 //Send and Get data from sever to database
 async function run() {
   try {
+    //All Database Collection
+    const DB = client.db("WorkSync");
+    const staffsCollection = DB.collection("staffs");
+
     //User Authentication Related API (JWT)
+    //jwt token setup
+    app.post("/jwt", (req, res) => {
+      const staff = req.body;
+      const token = jwt.sign(staff, process.env.ACCESS_TOKEN_KEY, {
+        expiresIn: "1h",
+      });
+
+      res.send({ token });
+    });
+
     //Admin Related API
+    //admin token verifying middleware
+    const verifyAdmin = async (req, res, next) => {
+      const uid = req.decoded.uid;
+      const result = await staffsCollection.findOne({ uid: uid });
+
+      if (!result || result?.role !== "Admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
+
+      next();
+    };
+
+    //HR Related API
+    //hr token verifying middleware
+    const verifyHR = async (req, res, next) => {
+      const uid = req.decoded.uid;
+      const result = await staffsCollection.findOne({ uid: uid });
+
+      if (!result || result?.role !== "HR") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
+
+      next();
+    };
+
     //Common API
+    //save new user data in database
+    app.put("/staffs", async (req, res) => {
+      const staff = req.body;
+      const query = { uid: staff?.uid };
+
+      //isExist already exist
+      const isExist = await staffsCollection.findOne(query);
+      if (isExist) {
+        return res.send({ message: "user already exist" });
+      }
+
+      //save new user
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          ...staff,
+        },
+      };
+      const result = await staffsCollection.updateOne(
+        query,
+        updateDoc,
+        options
+      );
+      res.send({ message: "saved new user data" });
+    });
   } catch (err) {
     console.log("Error from database:", err);
   }
